863aeea4fbd3ababe6e4c39ba7c17e7c528c27f3
Schueler209 <schueler109@jbs-pinneberg.de>
2019-09-20 09:29:57 +0200

    Leo Klassen

---
+++ form/connecter/ConnectorClient.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net.Sockets
+
+Public Class ConnectorClient
+    Private TcpClient As TcpClient
+
+
+
+
+
+    Public Sub connect()
+
+
+    End Sub
+
+
+    Public Sub send(sender As String, msg As String)
+
+    End Sub
+
+    Public Sub receive()
+
+    End Sub
+
+End Class
--- form/form/Form1.vb
+++ form/form/Form1.vb
@@ -2,7 +2,5 @@
 Imports connector
 
 Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
 End Class
